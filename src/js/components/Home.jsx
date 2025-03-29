ededf55a08f9bd1d681786b07152039e36511fdc
Alba-cdlg <alba.cdelagrana@icloud.com>
2025-03-29 09:34:46 +0000

    creada lista

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,58 @@
-import React from "react";
+import { useState } from "react";
+// import React { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [hoverIndex, setHoverIndex] = useState(null);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="container">
+      <h1>Lista</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                setTodos(todos.concat([inputValue]));
+                setInputValue("");
+              }
+            }}
+            placeholder="¿Qué tengo que hacer?"
+          />
+        </li>
+        {todos.map((item, index) => (
+          <li
+            key={index}
+            className={hoverIndex === index ? "todo-item hover" : "todo-item"}
+            onMouseEnter={() => setHoverIndex(index)}
+            onMouseLeave={() => setHoverIndex(null)}
+          >
+            {item}{" "}
+            {hoverIndex === index && (
+              <i
+                className="bi bi-x"
+                style={{ cursor: "pointer", color: "black" }}
+                onClick={() =>
+                  setTodos(
+                    todos.filter((t, currentIndex) => index != currentIndex)
+                  )
+                }
+              ></i>
+            )}
+          </li>
+        ))}
+      </ul>
+      <div>{todos.length} Tareas pendientes</div>
+    </div>
+  );
 };
 
 export default Home;
